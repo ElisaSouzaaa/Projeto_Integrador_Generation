9f147bb408fa8572489177caae25790033ebc0a3
raphaelfsg <raphaelfsg@gmail.com>
2023-10-04 08:58:02 -0300

    subindo arquivos de backend do projeto

---
+++ backend/src/main/java/com/agreenfood/agreenfood/AgreenfoodApplication.java
@@ -0,0 +1,13 @@
+package com.agreenfood.agreenfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreenfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgreenfoodApplication.class, args);
+	}
+
+}
