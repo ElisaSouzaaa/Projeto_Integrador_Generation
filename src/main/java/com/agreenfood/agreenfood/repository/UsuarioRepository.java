3f380d927a8a1984a374ff14c1850324fb41e7a1
raphaelfsg <raphaelfsg@gmail.com>
2023-10-16 13:23:38 -0300

    Changes by raquel

--- src/main/java/com/agreenfood/agreenfood/repository/UsuarioRepository.java
+++ src/main/java/com/agreenfood/agreenfood/repository/UsuarioRepository.java
@@ -1,13 +1,14 @@
 package com.agreenfood.agreenfood.repository;
 
-
 import com.agreenfood.agreenfood.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     public List<Usuario> findByNomeCompletoContainingIgnoreCase(@Param("nomeCompleto") String nomeCompleto);
 
